8a802db5334d6ac89bafe9ca66bd7ca36279bb01
runnerboy22 <runnerboy22@gmail.com>
2023-12-24 20:00:41 -0800

    metadata icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,15 @@ export const metadata: Metadata = {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
     },
   },
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/shortcut-icon.png',
+    apple: '/apple-icon.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/apple-touch-icon-precomposed.png',
+    },
+  },
 }
 
 export default function RootLayout({
