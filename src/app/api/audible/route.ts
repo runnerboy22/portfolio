c0b6555aaa5a9f44775ad7507c10ba0ba3d0fe39
runnerboy22 <runnerboy22@gmail.com>
2024-02-10 22:34:36 -0500

    change audible endpoint

--- src/app/api/audible/route.ts
+++ src/app/api/audible/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
-  const res = await fetch('http://localhost:5001/')
-  // const res = await fetch('https://audible-eosin.vercel.app/sms')
+  // const res = await fetch('http://localhost:5001/')
+  const res = await fetch('https://audible-eosin.vercel.app/')
   const data = await res.json()
   return Response.json(data)
 }
