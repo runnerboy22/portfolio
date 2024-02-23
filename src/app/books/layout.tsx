69f6f8524ab7176389176a24d183f71c2a6bdd14
runnerboy22 <runnerboy22@gmail.com>
2024-02-23 08:40:57 -0800

    fixed layout metadata

--- src/app/books/layout.tsx
+++ src/app/books/layout.tsx
@@ -2,14 +2,16 @@ import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Reading List',
-  icons: {
-    // icon: '/icon.png',
-    // shortcut: '/shortcut-icon.png',
-    apple: '/apple-touch-icon.png',
-    // other: {
-    //   rel: 'apple-touch-icon-precomposed',
-    //   url: '/apple-touch-icon-precomposed.png',
-    // },
-  },
+}
 
+export default function ReadingListLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+    </>
+  );
 }
