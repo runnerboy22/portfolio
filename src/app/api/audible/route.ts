4b9d57153a350795676117aac3a484dd9da3e147
Tyler <56234978+runnerboy22@users.noreply.github.com>
2024-03-02 21:08:54 +0000

    change link route

--- src/app/api/audible/route.ts
+++ src/app/api/audible/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  const res = await fetch('http://localhost:5001/')
+  const res = await fetch('https://audible-eosin.vercel.app/')
   // const res = await fetch('https://audible-eosin.vercel.app/')
   const data = await res.json()
   return Response.json(data)
